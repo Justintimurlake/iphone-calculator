6ddf4055827561f4b39772383a0debceb54f0d5f
TIMUR ADAMOV <99993754+Justintimurlake@users.noreply.github.com>
2023-03-22 15:39:23 -0700

    fix v
    i fixed bugs

--- basic-calculator/index.js
+++ basic-calculator/index.js
@@ -117,6 +117,205 @@
 //   inputFieldEl.value += buttonValue;
 // }
 
+//====================
+
+// class Calculator {
+//   constructor(inputFieldEl) {
+//     this.inputFieldEl = inputFieldEl;
+//     this.isPowerActive = false;
+//     this.base = null;
+//   }
+
+//   factorialRecursive(n) {
+//     if (n === 0 || n === 1) {
+//       return 1;
+//     } else {
+//       return n * this.factorialRecursive(n - 1);
+//     }
+//   }
+
+//   factorial() {
+//     const num = parseFloat(this.inputFieldEl.value);
+//     if (Number.isInteger(num) && num >= 0) {
+//       this.inputFieldEl.value = this.factorialRecursive(num);
+//     } else {
+//       alert("Please enter a non-negative integer value.");
+//     }
+//   }
+
+//   sqrt() {
+//     const num = parseFloat(this.inputFieldEl.value);
+//     if (num >= 0) {
+//       this.inputFieldEl.value = Math.sqrt(num);
+//     } else {
+//       alert("Please enter a non-negative value.");
+//     }
+//   }
+
+//   cbrt() {
+//     const num = parseFloat(this.inputFieldEl.value);
+//     this.inputFieldEl.value = Math.cbrt(num);
+//   }
+
+//   power() {
+//     if (this.inputFieldEl.value !== "") {
+//       this.base = parseFloat(this.inputFieldEl.value);
+//       this.inputFieldEl.value = "";
+//       this.isPowerActive = true;
+//     }
+//   }
+
+//   percent() {
+//     this.inputFieldEl.value =
+//       (parseFloat(this.inputFieldEl.value) / 100) *
+//       parseFloat(this.inputFieldEl.value);
+//   }
+
+//   clearResult() {
+//     this.inputFieldEl.value = "";
+//   }
+
+//   square() {
+//     this.inputFieldEl.value =
+//       parseFloat(this.inputFieldEl.value) ** 2;
+//   }
+
+//   sin() {
+//     this.inputFieldEl.value = Math.sin(
+//       parseFloat(this.inputFieldEl.value)
+//     );
+//   }
+
+//   cos() {
+//     this.inputFieldEl.value = Math.cos(
+//       parseFloat(this.inputFieldEl.value)
+//     );
+//   }
+
+//   addPi() {
+//     this.inputFieldEl.value += Math.PI;
+//   }
+
+//   log() {
+//     const num = parseFloat(this.inputFieldEl.value);
+//     if (num > 0) {
+//       this.inputFieldEl.value = Math.log10(num);
+//     } else {
+//       alert("Please enter a positive value.");
+//     }
+//   }
+
+//   calculateResult() {
+//     if (this.isPowerActive) {
+//       this.inputFieldEl.value = Math.pow(
+//         this.base,
+//         parseFloat(this.inputFieldEl.value)
+//       );
+//       this.isPowerActive = false;
+//     } else {
+//       this.inputFieldEl.value = eval(this.inputFieldEl.value);
+//     }
+//   }
+
+//   negate() {
+//     this.inputFieldEl.value = -parseFloat(this.inputFieldEl.value);
+//   }
+
+//   appendValue(buttonValue) {
+//     this.inputFieldEl.value += buttonValue;
+//   }
+// }
+
+// const inputFieldEl = document.getElementById("result");
+// const calculator = new Calculator(inputFieldEl);
+
+// const buttonsEl = document.querySelectorAll("button");
+
+// for (let i = 0; i < buttonsEl.length; i++) {
+//   buttonsEl[i].addEventListener("click", () => {
+//     const buttonValue = buttonsEl[i].textContent;
+//     if (buttonValue === "C") {
+//       calculator.clearResult();
+//     } else if (buttonValue === "=") {
+//       calculator.calculateResult();
+//     } else if (buttonValue === "+/-") {
+//       calculator.negate();
+//     } else if (buttonValue === "%") {
+//       calculator.percent();
+//     } else if (buttonValue === "sin") {
+//       calculator.sin();
+//     } else if (buttonValue === "cos") {
+//       calculator.cos();
+//     } else if (buttonValue === "x^2") {
+//       calculator.square();
+//     } else if (buttonValue === "y^x") {
+//       calculator.power();
+//     } else if (buttonValue === "!x") {
+//       calculator.factorial();
+//     } else if (buttonValue === "√x") {
+//       calculator.sqrt();
+//     } else if (buttonValue === "∛x") {
+//       calculator.cbrt();
+//     } else if (buttonValue === "log") {
+//       calculator.log();
+//     } else if (buttonValue === "π") {
+//       calculator.addPi();
+//     } else {
+//       calculator.appendValue(buttonValue);
+//     }
+//   });
+// }
+
+function handleKeyPress(event) {
+  const key = event.key;
+
+  const operatorKeys = ["+", "-", "*", "/"];
+
+  if (
+    operatorKeys.includes(key) &&
+    calculator.inputFieldEl.value === ""
+  ) {
+    alert("Please enter a positive value.");
+    return;
+  }
+  switch (key) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case ".":
+      calculator.appendValue(key);
+      break;
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      calculator.appendValue(key);
+      break;
+    case "Enter":
+      calculator.calculateResult();
+      break;
+    case "Backspace":
+    case "Delete":
+      calculator.clearResult();
+      break;
+    case "Escape":
+      calculator.clearResult();
+      break;
+    // Add more key codes for other functions as needed
+    default:
+      break;
+  }
+}
+
+document.addEventListener("keydown", handleKeyPress);
+
 class Calculator {
   constructor(inputFieldEl) {
     this.inputFieldEl = inputFieldEl;
@@ -125,7 +324,7 @@ class Calculator {
   }
 
   factorialRecursive(n) {
-    if (n === 0 || n === 1) {
+    if (n === 0 || n === 1 || n === "") {
       return 1;
     } else {
       return n * this.factorialRecursive(n - 1);
@@ -151,8 +350,12 @@ class Calculator {
   }
 
   cbrt() {
-    const num = parseFloat(this.inputFieldEl.value);
-    this.inputFieldEl.value = Math.cbrt(num);
+    if (this.inputFieldEl.value !== "") {
+      const num = parseFloat(this.inputFieldEl.value);
+      this.inputFieldEl.value = Math.cbrt(num);
+    } else {
+      alert("Please enter a positive value.");
+    }
   }
 
   power() {
@@ -160,13 +363,19 @@ class Calculator {
       this.base = parseFloat(this.inputFieldEl.value);
       this.inputFieldEl.value = "";
       this.isPowerActive = true;
+    } else {
+      alert("Please enter a positive value.");
     }
   }
 
   percent() {
-    this.inputFieldEl.value =
-      (parseFloat(this.inputFieldEl.value) / 100) *
-      parseFloat(this.inputFieldEl.value);
+    if (this.inputFieldEl.value > 0) {
+      this.inputFieldEl.value =
+        (parseFloat(this.inputFieldEl.value) / 100) *
+        parseFloat(this.inputFieldEl.value);
+    } else {
+      alert("Please enter a positive value.");
+    }
   }
 
   clearResult() {
@@ -174,20 +383,32 @@ class Calculator {
   }
 
   square() {
-    this.inputFieldEl.value =
-      parseFloat(this.inputFieldEl.value) ** 2;
+    if (this.inputFieldEl.value > 0) {
+      this.inputFieldEl.value =
+        parseFloat(this.inputFieldEl.value) ** 2;
+    } else {
+      alert("Please enter a positive value.");
+    }
   }
 
   sin() {
-    this.inputFieldEl.value = Math.sin(
-      parseFloat(this.inputFieldEl.value)
-    );
+    if (this.inputFieldEl.value > 0) {
+      this.inputFieldEl.value = Math.sin(
+        parseFloat(this.inputFieldEl.value)
+      );
+    } else {
+      alert("Please enter a positive value.");
+    }
   }
 
   cos() {
-    this.inputFieldEl.value = Math.cos(
-      parseFloat(this.inputFieldEl.value)
-    );
+    if (this.inputFieldEl.value > 0) {
+      this.inputFieldEl.value = Math.cos(
+        parseFloat(this.inputFieldEl.value)
+      );
+    } else {
+      alert("Please enter a positive value.");
+    }
   }
 
   addPi() {
@@ -232,6 +453,17 @@ const buttonsEl = document.querySelectorAll("button");
 for (let i = 0; i < buttonsEl.length; i++) {
   buttonsEl[i].addEventListener("click", () => {
     const buttonValue = buttonsEl[i].textContent;
+
+    const operatorKeys = ["+", "-", "*", "/", "="]; // Replace "×" and "÷" with "*" and "/" if you are using those symbols on your buttons.
+
+    if (
+      operatorKeys.includes(buttonValue) &&
+      calculator.inputFieldEl.value === ""
+    ) {
+      alert("Please enter a positive value.");
+      return;
+    }
+
     if (buttonValue === "C") {
       calculator.clearResult();
     } else if (buttonValue === "=") {
